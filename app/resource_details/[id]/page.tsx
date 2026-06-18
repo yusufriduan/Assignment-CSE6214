cba8a6a4058ca298ef16c7a9ec3a522984f57a2f
Shawn Huang Qi Yang <htp8090@gmail.com>
2026-06-18 10:13:11 +0800

    db setup

---
+++ app/resource_details/[id]/page.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { db } from "@/lib/DatabaseInitializer";
+import { collection, getDocs } from 'firebase/firestore/lite';
+import { useEffect } from "react";
+
+export default function ResourceDetails(){
+    // useEffect(() => {
+
+    // }, []);
+
+    return(
+        <div className="bg-background m-0 p-0 box-border flex flex-col">
+            
+        </div>
+    )
+}
