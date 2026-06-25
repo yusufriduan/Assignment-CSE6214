627aee34ee8bf1598b3a9513464aa539ffdd84c7
yusufriduan <yusufriduan@outlook.com>
2026-06-26 00:33:40 +0800

    temporarilt remove jwt trace for /dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,21 +1,27 @@
 import { BookingListUI } from "../components/BookingListUI";
 import StudentDashboard from "../components/Dashboard/StudentDashboard";
-import Login from "../login/page";
+
 interface PageProps {
   searchParams: Promise<{ default_sect?: string }>;
 }
 
 export default async function Dashboard({ searchParams }: PageProps) {
-  const userRole = "resourcemanager"; // This would typically come from your authentication logic
+
   const resolvedSearchParams = await searchParams;
   const default_sect = resolvedSearchParams.default_sect || null;
+
+  const userRole = 'student';
+
   if (userRole === "student") {
     return <StudentDashboard default_sect={default_sect} />;
-  } else if (userRole === "staff") {
+    
+  } else if (userRole === "campus staff" || userRole === "staff") {
     return <div>Staff Dashboard (to be implemented)</div>;
-  } else if (userRole === "resourcemanager"){
-    return <BookingListUI pageType="list"/>
+    
+  } else if (userRole === "resourcemanager") {
+    return <BookingListUI pageType="list"/>;
+    
   } else {
-    return <Login />;
+    return <div className="p-10 text-center text-red-500 font-bold">Unauthorized Role</div>;
   }
 }
